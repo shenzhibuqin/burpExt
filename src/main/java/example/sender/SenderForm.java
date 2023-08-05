208abb9f20d526a3f5faa91a23dc8e6df24314cb
nbb <nbbandczc@gmail.com>
2023-08-06 01:25:08 +0800

    v2.0

--- src/main/java/example/sender/SenderForm.java
+++ src/main/java/example/sender/SenderForm.java
@@ -9,14 +9,15 @@ import burp.api.montoya.ui.editor.HttpRequestEditor;
 import burp.api.montoya.ui.editor.HttpResponseEditor;
 
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.Objects;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class SenderForm {
 
@@ -24,7 +25,6 @@ public class SenderForm {
     public HttpResponseEditor responseViewer;
     public JPanel panel1;
     private final MontoyaApi api;
-    private JTextField textFieldUrl;
     private JCheckBox GETCheckBox;
     private JCheckBox POSTCheckBox;
     private JCheckBox otherCheckBox;
@@ -33,9 +33,12 @@ public class SenderForm {
     private JTextArea textAreaHeaders;
     private JTextArea textAreaBody;
     private JButton loadUrlsButton;
-    private JTextArea textAreaHosts;
+    private JTextArea textAreaUrls;
     private JTextField textPath;
     private JCheckBox pathCheckBox;
+    private JTextField textFieldThread;
+    private JLabel labelDone;
+    private JTextField textFieldTimeout;
 
     public SenderForm(HttpRequestEditor requestViewer, HttpResponseEditor responseViewer, MontoyaApi api) {
         this.requestViewer = requestViewer;
@@ -43,80 +46,17 @@ public class SenderForm {
         this.api = api;
         sendButton.addActionListener(e -> {
             Logging logging = api.logging();
+            labelDone.setText("0");
 
-            logging.logToOutput("-----prepare req-----");
-            if (otherCheckBox.isSelected() && textFieldMethod.getText().equals("")) {
-                logging.logToError("method is empty");
-                return;
+            if (textAreaUrls.getText().isBlank()){
+                logging.logToError("Hosts is empty");
+            }else if (textAreaUrls.getText().strip().contains("\n")){
+                logging.logToOutput("Batch req");
+                batchReq();
+            }else {
+                logging.logToOutput("Single req");
+                singleReq();
             }
-            String method;
-            if (GETCheckBox.isSelected()) {
-                method = "GET";
-            } else if (POSTCheckBox.isSelected()) {
-                method = "POST";
-            } else {
-                method = textFieldMethod.getText();
-            }
-            if (textAreaHosts.getText().equals("")) {
-                logging.logToError("url is empty");
-                return;
-            }
-            logging.logToOutput("method = " + method);
-
-            String url = textAreaHosts.getText().strip();
-            int index = url.indexOf("/", 8);
-            String serviceAddress = index == -1 ? url : url.substring(0, index);
-            HttpService service = HttpService.httpService(serviceAddress);
-            logging.logToOutput("service = " + serviceAddress);
-
-            String Host = serviceAddress.replace("https://", "").replace("http://", "");
-            logging.logToOutput("Host = " + Host);
-
-            String path;
-            if (pathCheckBox.isSelected() && !Objects.equals(pathCheckBox.getText().strip(), "")) {
-                path = index == -1 ? textPath.getText().strip() : url.substring(index) + textPath.getText().strip();
-            } else {
-                path = index == -1 ? "/" : url.substring(index);
-            }
-            logging.logToOutput("path = " + path);
-
-            HttpRequest req = HttpRequest.httpRequest()
-                    .withMethod(method)
-                    .withPath(path)
-                    .withService(service)
-                    .withAddedHeader("Host", Host)
-                    .withBody(textAreaBody.getText());
-
-            String headerString = textAreaHeaders.getText();
-            if (!headerString.equals("")) {
-                for (String header : headerString.split("\n")) {
-                    header = header.strip();
-                    index = header.indexOf(":");
-                    if (index == -1) {
-                        logging.logToError("invalid header: " + header);
-                        continue;
-                    }
-                    String k = header.substring(0, index).strip();
-                    String v = header.substring(index + 1).strip();
-
-                    if (!k.equals("Host")) {
-                        logging.logToOutput("add header " + k + ": " + v);
-                        req = req.withAddedHeader(k, v);
-                    } else {
-                        logging.logToOutput("update header " + k + ": " + v);
-                        req = req.withUpdatedHeader(k, v);
-                    }
-
-                }
-            }
-
-            logging.logToOutput("-----req prepared-----");
-            HttpRequest finalReq = req;
-            new Thread(() -> {
-                HttpRequestResponse res = api.http().sendRequest(finalReq);
-                requestViewer.setRequest(finalReq);
-                responseViewer.setResponse(res.response());
-            }).start();
         });
         GETCheckBox.addActionListener(e -> {
             POSTCheckBox.setSelected(false);
@@ -130,32 +70,228 @@ public class SenderForm {
             GETCheckBox.setSelected(false);
             POSTCheckBox.setSelected(false);
         });
-        loadUrlsButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                final JFileChooser fc = new JFileChooser();
-                fc.setFileSelectionMode(JFileChooser.FILES_ONLY);
-                // JFileChooser.FILES_ONLY
-                // JFileChooser.DIRECTORIES_ONLY
-                int returnVal = fc.showOpenDialog(panel1);
-                if (returnVal == JFileChooser.APPROVE_OPTION) {
-                    File file = fc.getSelectedFile();
-                    try {
-                        InputStreamReader read = new InputStreamReader(new FileInputStream(file), StandardCharsets.UTF_8);
-                        BufferedReader bufferedReader = new BufferedReader(read);
-                        StringBuilder urls = new StringBuilder();
-                        String lineText;
-                        while ((lineText = bufferedReader.readLine()) != null) {
-                            urls.append(lineText + "\n");
-                        }
-                        read.close();
-                        textAreaHosts.setText(urls.toString().strip());
-                    } catch (Exception exception) {
+        loadUrlsButton.addActionListener(e -> {
+            final JFileChooser fc = new JFileChooser();
+            fc.setFileSelectionMode(JFileChooser.FILES_ONLY);
+            // JFileChooser.FILES_ONLY
+            // JFileChooser.DIRECTORIES_ONLY
+            int returnVal = fc.showOpenDialog(panel1);
+            if (returnVal == JFileChooser.APPROVE_OPTION) {
+                File file = fc.getSelectedFile();
+                try {
+                    InputStreamReader read = new InputStreamReader(new FileInputStream(file), StandardCharsets.UTF_8);
+                    BufferedReader bufferedReader = new BufferedReader(read);
+                    StringBuilder urls = new StringBuilder();
+                    String lineText;
+                    while ((lineText = bufferedReader.readLine()) != null) {
+                        urls.append(lineText + "\n");
                     }
+                    read.close();
+                    textAreaUrls.setText(urls.toString().strip());
+                } catch (Exception exception) {
+                }
+            }
+
+        });
+    }
+
+    private void singleReq() {
+        Logging logging = api.logging();
+        logging.logToOutput("-----prepare req-----");
+        String method = getReqMethod();
+        if (method.isBlank()) {
+            logging.logToError("method is empty");
+            return;
+        }
+        logging.logToOutput("method = " + method);
+
+        String url = textAreaUrls.getText().strip();
+        int index = url.indexOf("/", 8);
+        String serviceAddress = index == -1 ? url : url.substring(0, index);
+        HttpService service = HttpService.httpService(serviceAddress);
+        logging.logToOutput("service = " + serviceAddress);
+
+        String Host = serviceAddress.replace("https://", "").replace("http://", "");
+        logging.logToOutput("Host = " + Host);
+
+        String path;
+        if (pathCheckBox.isSelected() && !Objects.equals(pathCheckBox.getText().strip(), "")) {
+            path = textPath.getText().strip();
+        } else {
+            path = index == -1 ? "/" : url.substring(index);
+        }
+        logging.logToOutput("path = " + path);
+
+        HttpRequest req = HttpRequest.httpRequest()
+                .withMethod(method)
+                .withPath(path)
+                .withService(service)
+                .withAddedHeader("Host", Host)
+                .withBody(textAreaBody.getText());
+
+        String headerString = textAreaHeaders.getText();
+        if (!headerString.isEmpty()) {
+            for (String header : headerString.strip().split("\n")) {
+                header = header.strip();
+                index = header.indexOf(":");
+                if (index == -1) {
+                    logging.logToError("invalid header: " + header);
+                    continue;
+                }
+                String k = header.substring(0, index).strip();
+                String v = header.substring(index + 1).strip();
+
+                if (!k.equals("Host")) {
+                    logging.logToOutput("add header " + k + ": " + v);
+                    req = req.withAddedHeader(k, v);
+                } else {
+                    logging.logToOutput("update header " + k + ": " + v);
+                    req = req.withUpdatedHeader(k, v);
                 }
 
             }
-        });
+        }
+
+        logging.logToOutput("-----req prepared-----");
+        HttpRequest finalReq = req;
+        new Thread(() -> {
+            HttpRequestResponse res = api.http().sendRequest(finalReq);
+            requestViewer.setRequest(finalReq);
+            responseViewer.setResponse(res.response());
+            labelDone.setText("1");
+        }).start();
+    }
+
+    private void batchReq() {
+        new Thread(() -> {
+            Logging logging = api.logging();
+            requestViewer.setRequest(null);
+            responseViewer.setResponse(null);
+            sendButton.setEnabled(false);
+            logging.logToOutput("Start! send button disable");
+
+            String method = getReqMethod();
+            if (method.isBlank()) {
+                logging.logToError("method is empty");
+                sendButton.setEnabled(true);
+                logging.logToOutput("Done! send button enable");
+                return;
+            }
+
+            ExecutorService pool = Executors.newFixedThreadPool(getThread());
+            String[] urls = textAreaUrls.getText().strip().split("\n");
+            for (String url : urls){
+                SwingWorker<Void, Void> worker = new SwingWorker<>() {
+                    @Override
+                    protected Void doInBackground() {
+                        int index = url.indexOf("/", 8);
+                        String serviceAddress = index == -1 ? url : url.substring(0, index);
+                        HttpService service = HttpService.httpService(serviceAddress);
+
+                        String Host = serviceAddress.replace("https://", "").replace("http://", "");
+
+                        String path;
+                        if (pathCheckBox.isSelected() && !Objects.equals(pathCheckBox.getText().strip(), "")) {
+                            path = textPath.getText().strip();
+                        } else {
+                            path = index == -1 ? "/" : url.substring(index);
+                        }
+
+                        HttpRequest req = HttpRequest.httpRequest()
+                                .withMethod(method)
+                                .withPath(path)
+                                .withService(service)
+                                .withAddedHeader("Host", Host)
+                                .withBody(textAreaBody.getText());
+
+                        String headerString = textAreaHeaders.getText();
+
+                        if (!headerString.isEmpty()) {
+                            for (String header : headerString.strip().split("\n")) {
+                                header = header.strip();
+                                index = header.indexOf(":");
+                                if (index == -1) {
+                                    logging.logToError("invalid header: " + header);
+                                    continue;
+                                }
+                                String k = header.substring(0, index).strip();
+                                String v = header.substring(index + 1).strip();
+
+                                if (!k.equals("Host")) {
+                                    req = req.withAddedHeader(k, v);
+                                } else {
+                                    req = req.withUpdatedHeader(k, v);
+                                }
+
+                            }
+                        }
+                        api.http().sendRequest(req);
+                        return null;
+                    }
+
+                    @Override
+                    protected void done() {
+                        int n = Integer.parseInt(labelDone.getText())+1;
+                        labelDone.setText(String.valueOf(n));
+                    }
+                };
+                //线程执行
+                pool.execute(worker);
+            }
+            pool.shutdown();
+            try {
+                if (!pool.awaitTermination(getTimeout(), TimeUnit.SECONDS)){
+                    logging.logToOutput("timeout");
+                    pool.shutdownNow();
+                }
+            } catch (InterruptedException e) {
+                pool.shutdownNow();
+            }
+            sendButton.setEnabled(true);
+            logging.logToOutput("Done! send button enable");
+        }).start();
+    }
+
+    private String getReqMethod() {
+        if (GETCheckBox.isSelected()) {
+            return  "GET";
+        } else if (POSTCheckBox.isSelected()) {
+            return  "POST";
+        } else {
+            return textFieldMethod.getText();
+        }
+    }
+
+    private  int getThread(){
+        try {
+            int Thread = Integer.parseInt(textFieldThread.getText());
+            if (Thread > 0){
+                return Thread;
+            }else {
+                return 10;
+            }
+        }
+        catch (Exception e){
+            textFieldThread.setText("10");
+            return 10;
+        }
+
+    }
+
+    private  int getTimeout(){
+        try {
+            int Thread = Integer.parseInt(textFieldTimeout.getText());
+            if (Thread > 0){
+                return Thread;
+            }else {
+                return 60;
+            }
+        }
+        catch (Exception e){
+            textFieldTimeout.setText("60");
+            return 60;
+        }
+
     }
 
 }
